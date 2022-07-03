1694d4b612b666e0c887d97f4c7261178c8e6bfe
NouraSaad19 <noura.saad1419@gmail.com>
2022-07-03 23:37:50 +0300

    hw2

--- todolist/src/App.js
+++ todolist/src/App.js
@@ -4,20 +4,38 @@ import { Component } from 'react';
 
 class App extends Component {
   
-  listAdd=()=>{
-    let textList = document.getElementById("text");
-    let ul = document.getElementById("list")
-    let li = document.createElement("li")
-    li.appendChild(document.createTextNode(textList.value))
-    ul.appendChild(li)
+  // listAdd=()=>{
+  //   let textList = document.getElementById("text");
+  //   let ul = document.getElementById("list")
+  //   let li = document.createElement("li")
+  //   li.appendChild(document.createTextNode(textList.value))
+  //   ul.appendChild(li)
+  // }
+
+
+  state={
+    tasks: []
   }
+
+  listAdd=()=>{
+  
+    this.setState({tasks: this.state.tasks.concat(document.getElementById("InputTask").value)});
+  }
+    
+  
   render(){
+    let todoList = this.state.tasks.map((task) => {
+      return <li>{task}</li>
+  
+    });
   return (
     <div className="App">
       <h1>TO DO LIST</h1>
-       <input id="text" type="text"/>
+       <input id="InputTask" type="text"/>
       <button onClick={this.listAdd} type="button">add</button>
-      <ul id="list"></ul>
+      <ul>
+        {todoList}
+      </ul>
     </div>
   );}
 }
